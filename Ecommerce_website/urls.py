067fdefb40a745869a039cb4da3003d34ec731f9
lionkiller900 <donalddaniels20@hotmail.com>
2021-01-07 17:26:15 +0000

    added content to front page

--- Ecommerce_website/urls.py
+++ Ecommerce_website/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('storage/', include('allauth.urls')),
+    path('', include('frontpage.url')),
 ]
