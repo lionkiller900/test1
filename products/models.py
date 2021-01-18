6a103e5d0ac7099833a47149dc52e1532f34f5a6
lionkiller900 <donalddaniels20@hotmail.com>
2021-01-18 23:47:30 +0000

    added poducts app and their contents

--- products/models.py
+++ products/models.py
@@ -17,9 +17,11 @@ class Category(models.Model):
 
 class Product(models.Model):
     categories = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    skus = models.CharField(max_length=254, null=True, blank=True)
+    skus = models.CharField(max_length=254, null=True, blank=True,)
     name = models.CharField(max_length=254)
     descriptions = models.TextField()
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    prices = models.DecimalField(max_digits=6, decimal_places=2, null=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
