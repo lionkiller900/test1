095f21d50e4a3a556e301ad2bf422578da4994f4
lionkiller900 <donalddaniels20@hotmail.com>
2021-01-21 21:21:42 +0000

    product page compatibility added

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -13,3 +13,14 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+def product_description(request, product_id):
+    """This is for viewing the product description"""
+
+    products = get_object_or_404(Product, id=product_id)
+
+    context = {
+        'products': products, 
+    }
+
+    return render(request, 'products/product_description.html', context)
