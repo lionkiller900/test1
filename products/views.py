9f00c5ca4d04200a57ec3b31896672647051608f
lionkiller900 <donalddaniels20@hotmail.com>
2021-01-26 16:20:46 +0000

    the capability of category products filtering

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -10,20 +10,27 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(categories__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'que' in request.GET:
             query = request.GET['que']
             if not query:
                 messages.error(request, "The search has not been entered.")
-                return redirect(reversal('products'))
+                return redirect(reverse('products'))
             
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = Q(name__icontains=query) | Q(descriptions__icontains=query)
             products = products.filter(queries)
 
     context = {
         'products': products,
         'seek_term': query,
+        'present_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
