29a375119eb2abd8977fae0d6b7f537ae0f1bbcb
lionkiller900 <donalddaniels20@hotmail.com>
2021-02-25 19:23:37 +0000

    started Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# About Ecommerce shoes
+This is the final project that is based on Django fundamentals in the Full Stack Developing Course.
+This is the website used for shoe purchase selection. 
--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItem(admin.ModelAdmin):
+    readonly_fields = ('product_number', 'date', 
+                    'cost_of_order', 'overall_cost', 
+                    'grand_cost')
+
+    fields = ('product_number', 'date', 'Name', 'email', 
+            'phone_number', 'home_Address', 'home_Address_continued', 
+            'postcode', 'county', 'country')
