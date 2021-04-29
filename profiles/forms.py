d94d8c84de256d14d3c297693037353649c7ce88
lionkiller900 <donalddaniels20@hotmail.com>
2021-04-29 20:52:31 +0000

    upgraded the profile page

---
+++ profiles/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    
+    def _init_(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_country': 'Country',
+            'default_postcode': 'PostCode',
+            'default_home_Address': 'Home Address 1',
+            'default_home_Address_continued': 'Home Address 2',
+            'default_county': 'County',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field in 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+            self.fields[field].label = False
