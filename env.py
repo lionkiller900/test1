f558c00d1dcf796f200e29240d2eb063203deda6
lionkiller900 <donalddaniels20@hotmail.com>
2021-05-01 15:58:31 +0000

    added the DATABASE_URL

--- env.py
+++ env.py
@@ -5,3 +5,4 @@ os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51IjVq8ANSSchvNACG3NkbGzuany4sXEzPZfC
 os.environ["STRIPE_SECRET_KEY"] = "sk_test_51IjVq8ANSSchvNACMnD3DRc3WVYVSeoELk0Cu4jpLXjNB6E7XZiBeySpUridTHBIJ1PYdBk1qWq8rXhgVppIci1U00lDnlWkvy"
 os.environ["STRIPE_WH_SECRET"] = "whsec_FSMkOVyGHo3GJHvuteonU7ncfqygcBHP"
 os.environ["DEVELOPMENT"] = "true"
+os.environ["DATABASE_URL"] = "postgres://ucqdclntavjqyo:11f795306b35b5ce8aca33ec22e5864efe9812722a98c0749e7e04938f678dde@ec2-54-74-156-137.eu-west-1.compute.amazonaws.com:5432/d20vlj8sds0db4"
