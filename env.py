51928a32f9c2ba9ac6c02dff5104ece5e7664d5b
lionkiller900 <donalddaniels20@hotmail.com>
2021-05-10 09:51:45 +0000

    removed env.py in git repo

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ["SECRET_KEY"] = "fshco+sgru6ol-1qnub*!y^b22la-znrde2c83n#!-qzf@8v$d"
-os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51IjVq8ANSSchvNACG3NkbGzuany4sXEzPZfCjePzFKs81MYok5zuWpqQ1kMtWUAZaVjU0vnPW2BHKZXYCmzegGlR00N91LyfTn"
-os.environ["STRIPE_SECRET_KEY"] = "sk_test_51IjVq8ANSSchvNACMnD3DRc3WVYVSeoELk0Cu4jpLXjNB6E7XZiBeySpUridTHBIJ1PYdBk1qWq8rXhgVppIci1U00lDnlWkvy"
-os.environ["STRIPE_WH_SECRET"] = "whsec_FSMkOVyGHo3GJHvuteonU7ncfqygcBHP"
-os.environ["DEVELOPMENT"] = "true"
-os.environ["DATABASE_URL"] = "postgres://ucqdclntavjqyo:11f795306b35b5ce8aca33ec22e5864efe9812722a98c0749e7e04938f678dde@ec2-54-74-156-137.eu-west-1.compute.amazonaws.com:5432/d20vlj8sds0db4"
